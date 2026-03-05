f1a0b20e7fca9b47e6f6dfd5eb1c0c305c5d9b5e
joantttgo <107sebastiangomez@gmail.com>
2026-03-05 16:59:13 -0500

    comentarios y aclaraciones de codigo

--- config/equipos/models.py
+++ config/equipos/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 class Equipo(models.Model):
     nombre = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, help_text="Ejemplo: boston-celtics")
-    logo = models.ImageField(upload_to='logos/')
-    color_principal = models.CharField(max_length=7, default="#007A33", help_text="Hexadecimal")
+    slug = models.SlugField(unique=True, help_text="Ejemplo: boston-celtics") #se usa para crear URL con el texto ingresado
+    logo = models.ImageField(upload_to='logos/') #para usar imagenes se instalo Pillow
+    color_principal = models.CharField(max_length=7, default="#007A33", help_text="Hexadecimal") #Color del equipo se usara para el color de la pagina
     descripcion = models.TextField()
 
     def __str__(self):
         return self.nombre
 
 class Jugador(models.Model):
-    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='jugadores')
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='jugadores') #permite que al crear jugador este se asocie a un equipo // Ademas de que si se elimina un equipo tambien se elimina sus jugadores 
     nombre = models.CharField(max_length=100)
-    numero = models.IntegerField()
+    numero = models.IntegerField() #IntegerField se usa para numeros enteros, no decimales, para decimales usar DecimalFiled
     posicion = models.CharField(max_length=50)
-    foto = models.ImageField(upload_to='jugadores/', null=True, blank=True)
+    foto = models.ImageField(upload_to='jugadores/', null=True, blank=True) #para usar imagenes se instalo Pillow
     es_estrella = models.BooleanField(default=False)
 
     def __str__(self):
--- config/equipos/urls.py
+++ config/equipos/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     # Ruta para el selector de equipos (página principal)
     path('', views.selector_equipos, name='selector'),
     
-    # Ruta dinámica para la home de cada equipo
+    # Ruta dinámica para la home de cada equipo, se evita crear html para cada equipo y asi usar una solo plantilla
     path('<slug:slug_equipo>/', views.home_equipo, name='home_equipo'),
     
     # Ruta para el roster del equipo
+    # Ruta dinámica para el roster de cada equipo, segun el slug definido en el admin
     path('<slug:slug_equipo>/roster/', views.roster_equipo, name='roster'),
 ]
